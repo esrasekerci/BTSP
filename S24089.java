9071011e5316398ca75fbf38012a786258b3cba9
esrasekerci <75069631+esrasekerci@users.noreply.github.com>
2020-12-28 00:04:17 +0300

    Update S24089.java

--- S24089.java
+++ S24089.java
@@ -1,3 +1,9 @@
+//p-büyülü sayılarını ekrana bastırmanızı istiyoruz. Bir sayının p-büyülü olabilmesi için aşağıdaki koşulları sağlaması gerekmektedir:
+//• p'den büyük olacak
+//• 2 ile p (dahil) arasında en az bir tam böleni olacak
+//• 1 fazlası 3’e tam bölünmeyecek
+//Programınız önce kullanıcıdan p sayısını alarak başlayacak. Daha sonra kullanıcıdan ikinci bir pozitif x tam sayısını alacaksınız.
+//Bu iki sayı alındıktan sonra ekrana x sayısından küçük eşit bütün p-büyülü sayılarını bastıracaksınız.
 
 import java.util.Scanner;
 
