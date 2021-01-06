410a48747531e33ca6a4ad0582bdc8c2292015d6
esrasekerci <75069631+esrasekerci@users.noreply.github.com>
2021-01-06 16:26:09 +0300

    Add files via upload

--- S24089.java
+++ S24089.java
@@ -1,30 +1,49 @@
-//p-büyülü sayılarını ekrana bastırmanızı istiyoruz. Bir sayının p-büyülü olabilmesi için aşağıdaki koşulları sağlaması gerekmektedir:
-//• p'den büyük olacak
-//• 2 ile p (dahil) arasında en az bir tam böleni olacak
-//• 1 fazlası 3’e tam bölünmeyecek
-//Programınız önce kullanıcıdan p sayısını alarak başlayacak. Daha sonra kullanıcıdan ikinci bir pozitif x tam sayısını alacaksınız.
-//Bu iki sayı alındıktan sonra ekrana x sayısından küçük eşit bütün p-büyülü sayılarını bastıracaksınız.
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class S24089 {
 
     public static void main(String[] args) {
-        int p, x, buyulu;
+        int n;
         Scanner values = new Scanner(System.in);
-        System.out.print("p sayisini giriniz: ");
-        p = values.nextInt();
-        System.out.print("x sayisini giriniz: ");
-        x = values.nextInt();
-        System.out.print("p-buyulu sayilar:");
-        for (buyulu = p + 1; buyulu <= x; buyulu++) {
-            for (int k = 2; k <= p; k++) {
-                if (((buyulu + 1) % 3 != 0) && (buyulu % k == 0)) {
-                    System.out.print(" " + buyulu);
+        System.out.print("n sayisini giriniz: ");
+        n = values.nextInt();
+        int[] dizi1 = new int[n];
+        boolean dizidevar = false;
+        int sira = 0;
+        int sonuc = 0;
+        System.out.print("dizi 1:");
+        do {
+            Random random = new Random();
+            int sayi = random.nextInt(26 + 25) - 25;
+            for (int i = 0; i < dizi1.length; i++) {
+                if (dizi1[i] == sayi) {
+                    dizidevar = true;
                     break;
                 }
             }
-
+            if (!dizidevar) {
+                dizi1[sira] = sayi;
+                sira++;
+            }
+            dizidevar = false;
+        } while (sira < n);
+        for (int i = 0; i < dizi1.length; i++) {
+            System.out.print(" " + dizi1[i]);
         }
+        for (int i = 0; i < dizi1.length; i++) {
+            if (sonuc == 0 && dizi1[i] < sonuc) {
+
+                sonuc = dizi1[i];
+            } else if (sonuc < dizi1[i] && dizi1[i] < 0) {
+
+                sonuc = dizi1[i];
+
+            }
+        }
+        System.out.println("\nsonuc: " + sonuc);
+
     }
+
 }
